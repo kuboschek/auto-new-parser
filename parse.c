c222921af61564e84a0f5a74b452b9cffcf59958
Leonhard <x32000@gmail.com>
2015-12-15 23:28:35 +0100

    Go back to IDLE state if no second STX. Also indent.

--- parse.c
+++ parse.c
@@ -1,9 +1,9 @@
 /*
- * parse.c
- *
- *  Created on: 12.08.2015
- *      Author: leonhard
- */
+* parse.c
+*
+*  Created on: 12.08.2015
+*      Author: leonhard
+*/
 
 
 #include "uart.h"
@@ -22,123 +22,126 @@ enum parse_state the_state = IDLE;
 uint8_t data_pos = 0;
 
 void print_byte(uint8_t byte) {
-        char str[3];
+  char str[3];
 
-        sprintf(str, "%X", byte);
-        uart1_puts(str);
+  sprintf(str, "%X", byte);
+  uart1_puts(str);
 }
 
 int parse_byte(frame_t* frm, uint8_t byte) {
-        if(frm == NULL)
-                return -1;
+  if(frm == NULL)
+  return -1;
 
-        switch(the_state) {
-        case IDLE:
-                if(byte == STX_VAL) {
-                        the_state = STX;
-                }
-                break;
-        case STX:
-                if(byte == STX_VAL) {
-                        the_state = LEN;
-                }
-                break;
+  switch(the_state) {
+    case IDLE:
+      if(byte == STX_VAL) {
+        the_state = STX;
+      }
+      break;
 
-        case LEN:
-                frm->data_len = byte;
-                data_pos = 0;
+    case STX:
+      if(byte == STX_VAL) {
+        the_state = LEN;
+      } else {
+        the_state = IDLE;
+      }
+      break;
 
-                frm->data = (char*) malloc(frm->data_len);
+    case LEN:
+      frm->data_len = byte;
+      data_pos = 0;
 
-                if(frm->data == NULL) {
-                        the_state = IDLE;
-                        return -1;
-                }
+      frm->data = (char*) malloc(frm->data_len);
 
-                the_state = DATA;
-                break;
+      if(frm->data == NULL) {
+        the_state = IDLE;
+        return -1;
+      }
 
-        case DATA:
-                /*
-                uart1_puts("datapos: ");
-                print_byte(data_pos);
-                uart1_puts(" inbyte: ");
-                print_byte(byte);
-                uart1_puts("\n");
-                */
+      the_state = DATA;
+      break;
 
-                frm->data[data_pos] = byte;
-                data_pos++;
+    case DATA:
+      /*
+      uart1_puts("datapos: ");
+      print_byte(data_pos);
+      uart1_puts(" inbyte: ");
+      print_byte(byte);
+      uart1_puts("\n");
+      */
 
-                if(data_pos == frm->data_len) {
-                        the_state = CRCH;
-                }
-                break;
+      frm->data[data_pos] = byte;
+      data_pos++;
 
-        case CRCH:
-                frm->crc = byte << 8;
+      if(data_pos == frm->data_len) {
+        the_state = CRCH;
+      }
+      break;
 
-                frm->id = frm->data[0];
+    case CRCH:
+      frm->crc = byte << 8;
 
-                the_state = CRCL;
-                break;
+      frm->id = frm->data[0];
 
-        case CRCL:
-                frm->crc |= (byte & 0xFF);
-                uint16_t calc_crc = 0xFFFF;
+      the_state = CRCL;
+      break;
 
-                for(int i = 0; i < frm->data_len; i++) {
-                        calc_crc = _crc16_update(calc_crc, frm->data[i]);
-                }
+    case CRCL:
+      frm->crc |= (byte & 0xFF);
+      uint16_t calc_crc = 0xFFFF;
 
-                //dbg_info("Msg CRC is: %d, Calc CRC is: %d", frm->crc, calc_crc);
+      for(int i = 0; i < frm->data_len; i++) {
+        calc_crc = _crc16_update(calc_crc, frm->data[i]);
+      }
 
-                the_state = IDLE;
+      //dbg_info("Msg CRC is: %d, Calc CRC is: %d", frm->crc, calc_crc);
 
-                if(calc_crc != frm->crc) {
-                        uart1_puts("Incorrect CRC.\n");
-                }
+      the_state = IDLE;
 
-                if(calc_crc == frm->crc)
-                        return 1;
-                else
-                        return -1;
+      if(calc_crc != frm->crc) {
+        uart1_puts("Incorrect CRC.\n");
+      }
 
-                break;
-        }
+      if(calc_crc == frm->crc)
+        return 1;
+      else
+        return -1;
 
-        return 0;
+      break;
+  }
+
+  return 0;
 }
 
 int send_msg(frame_t* frm, void (*send_byte)(uint8_t)) {
-        if(frm == NULL)
-                return -1;
+  if(frm == NULL)
+  return -1;
 
-        //Update CRC
-        uint16_t calc_crc = 0xFFFF;
-
-
-        for(int i=0; i < frm->data_len; i++) {
-                calc_crc = _crc16_update(calc_crc, frm->data[i]);
-        }
-
-        frm->crc = calc_crc;
-
-        //STX
-        send_byte(STX_VAL);
-        send_byte(STX_VAL);
-
-        //LEN
-        send_byte(frm->data_len);
-
-        //DATA
-        for(int i = 0; i < frm->data_len; i++)
-                send_byte(frm->data[i]);
-
-        //CRC
-        send_byte((char) (frm->crc >> 8));
-        send_byte((char) (frm->crc & 0xFF));
+  //Update CRC
+  uint16_t calc_crc = 0xFFFF;
 
 
-        return 1;
+  for(int i=0; i < frm->data_len; i++) {
+    calc_crc = _crc16_update(calc_crc, frm->data[i]);
+  }
+
+  frm->crc = calc_crc;
+
+  //STX
+  send_byte(STX_VAL);
+  send_byte(STX_VAL);
+
+  //LEN
+  send_byte(frm->data_len);
+
+  //DATA
+  for(int i = 0; i < frm->data_len; i++)
+  send_byte(frm->data[i]);
+
+  //CRC
+  send_byte((char) (frm->crc >> 8));
+  send_byte((char) (frm->crc & 0xFF));
+
+
+  return 1;
 }
--- parse.h
+++ parse.h
@@ -1,9 +1,9 @@
 /*
- * parse.h
- *
- *  Created on: 12.08.2015
- *      Author: leonhard
- */
+* parse.h
+*
+*  Created on: 12.08.2015
+*      Author: leonhard
+*/
 
 #ifndef PARSE_H_
 #define PARSE_H_
@@ -11,20 +11,20 @@
 #include <stdint.h>
 
 enum parse_state {
-        IDLE,   // First STX -> STX state
-        STX,    // Second STX -> LEN state
-        LEN,    // Length byte -> DATA state
-        DATA,   // More data -> DATA state, last data -> CRCH state
-        CRCH,   // CRCH byte -> CRCL state
-        CRCL    // CRCL byte -> check CRC, IDLE state
+  IDLE,   // First STX -> STX state
+  STX,    // Second STX -> LEN state
+  LEN,    // Length byte -> DATA state
+  DATA,   // More data -> DATA state, last data -> CRCH state
+  CRCH,   // CRCH byte -> CRCL state
+  CRCL    // CRCL byte -> check CRC, IDLE state
 };
 
 typedef struct {
-        uint8_t id;
-        uint8_t data_len;
-        char* data;
+  uint8_t id;
+  uint8_t data_len;
+  char* data;
 
-        uint16_t crc;
+  uint16_t crc;
 } frame_t;
 
 int parse_byte(frame_t* frm, uint8_t byte);
