6602b8094ce9f4eb6dd3833dc85e10f0e6c4af4e
bklw <x32000@gmail.com>
2015-12-26 22:49:38 +0100

    Fix handle files

--- handle.c
+++ handle.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include "parse.h"
+
 /*[[[cog
 import cog
 import json
@@ -18,10 +21,14 @@ cog.outl("#define LAST_MSG_ID %s" % last_msg_id)
 ]]]*/
 //[[[end]]]
 
-
-void (*handlers[LAST_MSG_ID + 1])(frame_t*) = NULL;
+void (*handlers[LAST_MSG_ID + 1])(frame_t*) = {NULL};
 void (*default_handler)(frame_t*) = NULL;
 
+void init_handlers() {
+   for(int i = 0; i < LAST_MSG_ID + 1; i++) {
+     handlers[i] = NULL;
+   }
+}
 
 void set_handler(uint8_t msg_id, void (*handler)(frame_t*)) {
   if(msg_id <= LAST_MSG_ID)
@@ -33,7 +40,7 @@ void set_default_handler(void (*handler)(frame_t*)) {
 }
 
 void handle_frame(frame_t *frm) {
-  if((frm->id > LAST_MSG_ID)
+  if((frm->id > LAST_MSG_ID))
     return;
 
   if(handlers[frm->id] != NULL)
--- handle.h
+++ handle.h
@@ -3,6 +3,10 @@
 #ifndef _HANDLE_H_
 #define _HANDLE_H_
 
+#include "parse.h"
+
+void init_handlers();
+
 void set_handler(uint8_t msg_id, void (*handler)(frame_t*));
 
 void set_default_handler(void (*handler)(frame_t*));
